f29297cba3b6f1fb4ef1fa43cbd6f7ed4559bb43
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-03-03 15:14:43 +0900

    다른풀이

--- exercism/nthPrime.js
+++ exercism/nthPrime.js
@@ -20,3 +20,38 @@ const prime = (nth) => {
     if (cnt === nth - 1) return i;
   }
 };
+
+//  sol2) generator를 사용하여 풀어보자!
+function* generatePrimeArr() {
+  let num = 2;
+  while (true) {
+    let isPrime = true;
+    if (num === 2) yield num;
+    if (num > 2) {
+      for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        yield num;
+      }
+    }
+
+    num++;
+  }
+}
+
+const prime2 = (numIndex) => {
+  if (numIndex === 0) throw new Error("there is no zeroth prime");
+  const res = generatePrimeArr();
+  const primeArr = [];
+
+  while (true) {
+    primeArr.push(res.next().value);
+    if (numIndex === primeArr.length) {
+      return primeArr[numIndex - 1];
+    }
+  }
+};
