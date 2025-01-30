738b2c91f5599bb560d23c0b244c44b7d9f353f4
yunajoe <yunajoe@gmail.com>
2025-01-30 12:54:43 +0900

    feat: binarySearch.js

---
+++ leetcode/easy/binarySearch.js
@@ -0,0 +1,36 @@
+//  해당 index를 return
+const binarySearch = (arr, targetNumber) => {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex + 1 < rightIndex) {
+    let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (arr[midIndex] === targetNumber) return midIndex;
+
+    if (arr[midIndex] < targetNumber) {
+      leftIndex = midIndex;
+    }
+    if (arr[midIndex] > targetNumber) {
+      rightIndex = midIndex;
+    }
+  }
+
+  //   왼쪽 인덱수
+  if (arr[leftIndex] === targetNumber) {
+    return leftIndex;
+  }
+
+  //   오른쪽 인덱수
+  if (arr[rightIndex] === targetNumber) {
+    return rightIndex;
+  }
+
+  return -1;
+};
+
+const nums = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59];
+
+// console.log(`Index of 37 is ---> ${binarySearch(nums, 37)}`);
+// console.log(`Index of 1 is ---> ${binarySearch(nums, 1)}`);
+// console.log(`Index of 59 is ---> ${binarySearch(nums, 59)}`);
+// console.log(`Index of 25 is ---> ${binarySearch(nums, 25)}`);
