04877b18a1a77e754a7409a35974f1000a229300
yunajoe0620 <yunajoe@fineteacher.com>
2025-05-01 07:31:50 +0900

    IntegerToRoman.js

--- leetcode/medium/IntegerToRoman.js
+++ leetcode/medium/IntegerToRoman.js
@@ -78,3 +78,29 @@ var intToRoman = function (num) {
   });
   return answer;
 };
+
+var intToRoman = function (num) {
+  let answer = "";
+  const arr = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+
+  for (const [value, symbol] of arr) {
+    let count = Math.floor(num / value);
+    answer += symbol.repeat(count);
+    num = num - value * count;
+  }
+  return answer;
+};
