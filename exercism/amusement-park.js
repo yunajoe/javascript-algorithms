eb43538d9a66f4d8c2aa4bb20227eb99d3fbf99e
yunajoe <yunajoe@gmail.com>
2025-10-02 08:35:43 +0900

    amusement-park.js

---
+++ exercism/amusement-park.js
@@ -0,0 +1,72 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+  return {
+    name,
+    age,
+    ticketId,
+  };
+}
+
+/**
+ * Revokes a ticket for a visitor.
+ *
+ * @param {Visitor} visitor the visitor with an active ticket
+ * @returns {Visitor} the visitor without a ticket
+ */
+export function revokeTicket(visitor) {
+  visitor.ticketId = null;
+  return visitor;
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function ticketStatus(tickets, ticketId) {
+  const keys = Object.keys(tickets);
+  if (!keys.includes(ticketId)) return "unknown ticket id";
+  if (!tickets[ticketId]) return "not sold";
+  return `sold to ${tickets[ticketId]}`;
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object
+ * and returns a simplified status message.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function simpleTicketStatus(tickets, ticketId) {
+  const keys = Object.keys(tickets);
+  // !tickets[ticketID] 하지 않은 이유는 falsy값이 다 들어가서, (0, "")이 다 속해 있는다.
+  if (!keys.includes(ticketId) || tickets[ticketId] === null)
+    return "invalid ticket !!!";
+  return tickets[ticketId];
+}
+
+/**
+ * Determines the version of the GTC that was signed by the visitor.
+ *
+ * @param {VisitorWithGtc} visitor
+ * @returns {string | undefined} version
+ */
+
+export function gtcVersion(visitor) {
+  const gtc = "gtc";
+  if (!visitor[gtc]) return undefined;
+  return visitor[gtc].version;
+}
