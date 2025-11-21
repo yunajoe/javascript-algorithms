1e42bbca47158a8a8f047bf948cec59a61908053
yunajoe <yunajoe@gmail.com>
2025-11-21 10:10:37 +0900

    minMaxSum.js

--- hackerrank/easy/minMaxSum.js
+++ hackerrank/easy/minMaxSum.js
@@ -1,16 +1,13 @@
+const calculteSum = (arr) => {
+  return arr.reduce((acc, value) => {
+    acc += value;
+    return acc;
+  }, 0);
+};
 function miniMaxSum(arr) {
   // Write your code here
-  let answer = 0;
-  let answer2 = 0;
-  const asendingArr = [...arr].sort((a, b) => a - b);
-  const desendingArr = [...arr].sort((a, b) => b - a);
-  for (let i = 0; i <= 3; i++) {
-    answer += asendingArr[i];
-  }
-
-  for (let i = 0; i <= 3; i++) {
-    answer2 += desendingArr[i];
-  }
-
-  console.log(answer, answer2);
+  const sorted = [...arr].sort((a, b) => a - b);
+  const minSum = calculteSum(sorted.slice(0, -1));
+  const maxSum = calculteSum(sorted.slice(1));
+  console.log(minSum, maxSum);
 }
