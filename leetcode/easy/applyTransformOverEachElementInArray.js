08c0a091ee039d4a6e7894599bc4e1f85173f9f5
yunajoe <yunajoe@gmail.com>
2026-02-02 22:46:58 +0900

    refactor: applyTransformOverEachElementInArray

--- leetcode/easy/applyTransformOverEachElementInArray.js
+++ leetcode/easy/applyTransformOverEachElementInArray.js
@@ -1,22 +1,12 @@
 /**
- *@param{number[]} arr
- *@param{Function} fn
- *@return{number[]}
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
  */
-
-const map = function (arr, fn) {
-  let result = [];
-  arr.forEach((num, idx) => {
-    let value = fn(num, idx);
-    result.push(value);
-  });
-  return result;
-};
-
-const map2 = function (arr, fn) {
-  const arrCopy = [...arr];
-  arrCopy.forEach((num, idx) => {
-    arrCopy[idx] = fn(num, idx);
-  });
-  return arrCopy;
+var map = function (arr, fn) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(fn(arr[i], i));
+  }
+  return newArr;
 };
