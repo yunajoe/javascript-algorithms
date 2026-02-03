7821bc7ad070189d2835fa4163e2252bf827d4a2
yunajoe <yunajoe@gmail.com>
2026-02-03 21:59:02 +0900

    refactor: filterELementsFromArray

--- leetcode/easy/filterElementsFromArray.js
+++ leetcode/easy/filterElementsFromArray.js
@@ -4,21 +4,13 @@
  * @return {number[]}
  */
 
-const filter = function (arr, fn) {
-  const arrCopy = [...arr];
-  const removedIdx = [];
-  const result = [];
-  for (let i = 0; i < arrCopy.length; i++) {
-    let result = fn(arrCopy[i], i);
-    if (!result) {
-      removedIdx.push(i);
+var filter = function (arr, fn) {
+  const answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = fn(arr[i], i);
+    if (result) {
+      answer.push(arr[i]);
     }
   }
-
-  for (let i = 0; i < arrCopy.length; i++) {
-    if (!removedIdx.includes(i)) {
-      result.push(arrCopy[i]);
-    }
-  }
-  return result;
+  return answer;
 };
