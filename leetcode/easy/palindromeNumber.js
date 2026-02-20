373fbfea9eeadaf15830ccbb3d03293f2138323a
yunajoe <yunajoe@gmail.com>
2026-02-20 20:19:59 +0900

    palindromNumber

--- leetcode/easy/palindromeNumber.js
+++ leetcode/easy/palindromeNumber.js
@@ -2,12 +2,10 @@
  * @param {number} x
  * @return {boolean}
  */
-
 var isPalindrome = function (x) {
   const str = String(x);
-  let length = str.length - 1;
   for (let i = 0; i < str.length; i++) {
-    if (str[i] !== str[length - i]) {
+    if (i < str.length - 1 - i && str[i] !== str[str.length - 1 - i]) {
       return false;
     }
   }
