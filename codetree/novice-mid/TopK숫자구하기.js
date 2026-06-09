5d7457fc65ba6bbd570b8d85cb1574151a82c13b
yunajoe <yunajoe@gmail.com>
2026-06-09 09:02:48 +0900

    topK숫자구하기

---
+++ codetree/novice-mid/TopK숫자구하기.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [n, k] = input[0].split(" ").map(Number);
+const nums = input[1].trim().split(" ").map(Number);
+
+// Please Write your code here.
+const sortedArr = [...nums].sort((a, b) => a - b);
+
+console.log(sortedArr[k - 1]);
