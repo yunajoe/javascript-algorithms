50a9dc21243ad37f9d2bcfe2af0d20c99d627361
yunajoe <yunajoe@gmail.com>
2026-08-14 20:30:06 +0900

    reafctor:이상한진수2

--- codetree/novice-mid/이상한진수2.js
+++ codetree/novice-mid/이상한진수2.js
@@ -5,25 +5,13 @@ const a = input[0];
 let arr = a.split("");
 let maxNum = 0;
 
-// Please Write your code here.
-const getNumber = (arr) => {
-  let num = 0;
-  for (let i = arr.length - 1; i >= 0; i--) {
-    // 3 2 1 0
-    const value = Number(arr[arr.length - 1 - i]);
-    num += Math.pow(2, i) * value;
-  }
-  return num;
-};
-
 for (let i = 1; i < arr.length; i++) {
-  if (arr[i] === "0") {
-    arr[i] = "1";
-  } else {
-    arr[i] = "0";
-  }
-  const value = getNumber(arr);
-  maxNum = Math.max(value, maxNum);
-  arr = a.split("");
+  arr[i] = arr[i] === "0" ? "1" : "0";
+
+  const currentNum = parseInt(arr.join("", 2));
+  maxNum = Math.max(maxNum, currentNum);
+
+  arr[i] = arr[i] === "0" ? "1" : "0";
 }
+
 console.log(maxNum);
