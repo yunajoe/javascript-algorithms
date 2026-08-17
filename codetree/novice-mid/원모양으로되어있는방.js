93282143256de1f31b17d8ac94f402d0d9588796
yunajoe <yunajoe@gmail.com>
2026-08-17 12:24:22 +0900

    refactor:원모양으로되어있는방

--- codetree/novice-mid/원모양으로되어있는방.js
+++ codetree/novice-mid/원모양으로되어있는방.js
@@ -4,21 +4,14 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
-let shortest_distance = Number.MAX_SAFE_INTEGER;
-for (let i = 0; i < arr.length; i++) {
-  let sum = 0;
-  for (let j = 0; j < arr.length; j++) {
-    if (i === j) continue;
-    if (i > j) {
-      const value = (j + (n - 1 - i + 1)) * arr[j];
-      sum += value;
-    }
-    if (i < j) {
-      const value = (j - i) * arr[j];
-      sum += value;
-    }
+let minDistance = Number.MAX_SAFE_INTEGER;
+for (let i = 0; i < n; i++) {
+  let totalDist = 0;
+  for (let j = 0; j < n; j++) {
+    const dist = (j - i + n) % n;
+    totalDist += dist * arr[j];
   }
-  shortest_distance = Math.min(sum, shortest_distance);
+  minDistance = Math.min(minDistance, totalDist);
 }
 
-console.log(shortest_distance);
+console.log(console.log(minDistance));
