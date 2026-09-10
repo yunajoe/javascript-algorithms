c12828a7e5261b91bdfe9b0cee89ca8762435276
yunajoe <yunajoe@gmail.com>
2026-09-10 09:33:30 +0900

    삽입정렬구현방법 추가

--- codetree/novice-high/삽입정렬구현.js
+++ codetree/novice-high/삽입정렬구현.js
@@ -5,8 +5,7 @@ const n = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
 
 // Please Write your code here.
-
-// key
+// for문
 for (let i = 1; i < arr.length; i++) {
   // 비교해야 할 대상
   const key = arr[i];
@@ -21,4 +20,18 @@ for (let i = 1; i < arr.length; i++) {
     arr[swapIndex] = key;
   }
 }
+console.log(arr.join(" "));
+
+// while문
+
+for (let i = 1; i < arr.length; i++) {
+  const key = arr[i]; // 1
+  let compareIndex = i - 1;
+  while (compareIndex >= 0 && arr[compareIndex] > key) {
+    arr[compareIndex + 1] = arr[compareIndex];
+    compareIndex--; // 5
+  }
+  arr[compareIndex + 1] = key;
+}
+
 console.log(arr.join(" "));
